9a1fb5a0c60ecc35e47ac6ed7575593ba8f6dcc4
Eliott <mauberteliott@gmail.com>
2025-02-18 16:02:30 +0100

    feat: add new project images and logos to public directory

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,20 +1,20 @@
 export const projects = [
     {
-      preview: "/src/assets/project/music-together-project.webp",
+      preview: "/project/music-together-project.webp",
       name: "Music together",
       description: "Application web permettant l'écoute collaborative de musique en temps réel. Les utilisateurs peuvent créer ou rejoindre une salle via un code unique, partager des liens YouTube pour diffuser des morceaux synchronisés et discuté via un chat intégré.",
       langage: ["react", "nodejs", "tailwind"],
       githubUrl: "https://github.com/Eliott133/music-together",
     },
     {
-      preview: "/src/assets/project/voxceleb-project.png",
+      preview: "/project/voxceleb-project.png",
       name: "Évaluation de l'interprétabilité des représentations vectorielles",
       description: "Projet de recherche avec le LIUM sur l'évaluation des représentations neuronales en identification du locuteur. Développement de filtres audio et analyse des dimensions du modèle SPINE pour améliorer l'interprétabilité.",
       langage: ["react", "postgresql", "nodejs", "tailwind", "python"],
       githubUrl: "https://github.com/Eliott133/VoxCeleb1",
     },
     {
-      preview: "/src/assets/project/maze-simulator-project.webp",
+      preview: "/project/maze-simulator-project.webp",
       name: "Maze simulator",
       description: "Simulateur de labyrinthe avec différents algorithmes de génération et de résolution.",
       langage: ["react", "vite"],
@@ -22,22 +22,22 @@ export const projects = [
       demoUrl: "https://maze-simulator-eliott.netlify.app/"
     },
     {
-      preview: "/src/assets/project/portfolio.webp",
+      preview: "/project/portfolio.webp",
       name: "Portfolio",
       description: "Création de mon portfolio pour exposer mon cv différements",
       langage: ["react", "vite", "tailwind"],
       githubUrl: "https://github.com/Eliott133/portfolio",
-      demoUrl: "https://maze-simulator-eliott.netlify.app/"
+      demoUrl: "https://eliott-portfolio.netlify.app/"
     },
     {
-      preview: "/src/assets/project/movie-app.png",
+      preview: "/project/movie-app.png",
       name: "Movie App",
       description: "Movie App est une application React qui vous permet de rechercher des films, de les ajouter à une liste de favoris et de les afficher. Elle utilise l'API The Movie Database (TMDb) pour rechercher et afficher des films, et React Context pour gérer le statut des films favoris.",
       langage: ["react", "vite", "tailwind"],
       githubUrl: "https://github.com/Eliott133/db-movie",
     },
     {
-      preview: "/src/assets/project/password-generator.png",
+      preview: "/project/password-generator.png",
       name: "Password generator",
       description: "Ce projet est une application web permettant de générer des mots de passe personnalisés en fonction de différents critères. Grâce à une interface conviviale, l'utilisateur peut choisir la longueur du mot de passe et sélectionner les types de caractères à inclure (minuscules, majuscules, chiffres et caractères spéciaux).",
       langage: ["react", "vite", "tailwind"],
