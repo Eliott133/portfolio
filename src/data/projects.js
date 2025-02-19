deaf02221a2d3d9a3fedf731212090a13315eb47
Eliott <mauberteliott@gmail.com>
2025-02-19 13:35:05 +0100

    feat: add Minimal Rental Manager project to projects list and include associated image

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,54 @@
 export const projects = [
-    {
-      preview: "/project/music-together-project.webp",
-      name: "Music together",
-      description: "Application web permettant l'écoute collaborative de musique en temps réel. Les utilisateurs peuvent créer ou rejoindre une salle via un code unique, partager des liens YouTube pour diffuser des morceaux synchronisés et discuté via un chat intégré.",
-      langage: ["react", "nodejs", "tailwind"],
-      githubUrl: "https://github.com/Eliott133/music-together",
-    },
-    {
-      preview: "/project/voxceleb-project.png",
-      name: "Évaluation de l'interprétabilité des représentations vectorielles",
-      description: "Projet de recherche avec le LIUM sur l'évaluation des représentations neuronales en identification du locuteur. Développement de filtres audio et analyse des dimensions du modèle SPINE pour améliorer l'interprétabilité.",
-      langage: ["react", "postgresql", "nodejs", "tailwind", "python"],
-      githubUrl: "https://github.com/Eliott133/VoxCeleb1",
-    },
-    {
-      preview: "/project/maze-simulator-project.webp",
-      name: "Maze simulator",
-      description: "Simulateur de labyrinthe avec différents algorithmes de génération et de résolution.",
-      langage: ["react", "vite"],
-      githubUrl: "https://github.com/Eliott133/maze-simulator",
-      demoUrl: "https://maze-simulator-eliott.netlify.app/"
-    },
-    {
-      preview: "/project/portfolio.webp",
-      name: "Portfolio",
-      description: "Création de mon portfolio pour exposer mon cv différements",
-      langage: ["react", "vite", "tailwind"],
-      githubUrl: "https://github.com/Eliott133/portfolio",
-      demoUrl: "https://eliott-portfolio.netlify.app/"
-    },
-    {
-      preview: "/project/movie-app.png",
-      name: "Movie App",
-      description: "Movie App est une application React qui vous permet de rechercher des films, de les ajouter à une liste de favoris et de les afficher. Elle utilise l'API The Movie Database (TMDb) pour rechercher et afficher des films, et React Context pour gérer le statut des films favoris.",
-      langage: ["react", "vite", "tailwind"],
-      githubUrl: "https://github.com/Eliott133/db-movie",
-    },
-    {
-      preview: "/project/password-generator.png",
-      name: "Password generator",
-      description: "Ce projet est une application web permettant de générer des mots de passe personnalisés en fonction de différents critères. Grâce à une interface conviviale, l'utilisateur peut choisir la longueur du mot de passe et sélectionner les types de caractères à inclure (minuscules, majuscules, chiffres et caractères spéciaux).",
-      langage: ["react", "vite", "tailwind"],
-      githubUrl: "https://github.com/Eliott133/password_generator",
-      demoUrl: "https://generate-your-password-eliott.netlify.app/"
-    }
-  ];
+  {
+    preview: "/project/minimal-rental-manager.png",
+    name: "Minimal rental manager",
+    description: "Un système de gestion immobilière moderne et simplifié. L'application a été conçue pour aider les propriétaires à gérer efficacement leurs biens, leurs locataires et leurs documents.",
+    langage: ["react", "postgresql", "tailwind"],
+    githubUrl: "https://github.com/Eliott133/minimal-rental-manager",
+  },
+  {
+    preview: "/project/voxceleb-project.png",
+    name: "Évaluation de l'interprétabilité des représentations vectorielles",
+    description: "Projet de recherche avec le LIUM sur l'évaluation des représentations neuronales en identification du locuteur. Développement de filtres audio et analyse des dimensions du modèle SPINE pour améliorer l'interprétabilité.",
+    langage: ["react", "postgresql", "nodejs", "tailwind", "python"],
+    githubUrl: "https://github.com/Eliott133/VoxCeleb1",
+  },
+  {
+    preview: "/project/maze-simulator-project.webp",
+    name: "Maze simulator",
+    description: "Simulateur de labyrinthe avec différents algorithmes de génération et de résolution.",
+    langage: ["react", "vite"],
+    githubUrl: "https://github.com/Eliott133/maze-simulator",
+    demoUrl: "https://maze-simulator-eliott.netlify.app/"
+  },
+  {
+    preview: "/project/portfolio.webp",
+    name: "Portfolio",
+    description: "Création de mon portfolio pour exposer mon cv différements",
+    langage: ["react", "vite", "tailwind"],
+    githubUrl: "https://github.com/Eliott133/portfolio",
+    demoUrl: "https://eliott-portfolio.netlify.app/"
+  },
+  {
+    preview: "/project/movie-app.png",
+    name: "Movie App",
+    description: "Movie App est une application React qui vous permet de rechercher des films, de les ajouter à une liste de favoris et de les afficher. Elle utilise l'API The Movie Database (TMDb) pour rechercher et afficher des films, et React Context pour gérer le statut des films favoris.",
+    langage: ["react", "vite", "tailwind"],
+    githubUrl: "https://github.com/Eliott133/db-movie",
+  },
+  {
+    preview: "/project/password-generator.png",
+    name: "Password generator",
+    description: "Ce projet est une application web permettant de générer des mots de passe personnalisés en fonction de différents critères. Grâce à une interface conviviale, l'utilisateur peut choisir la longueur du mot de passe et sélectionner les types de caractères à inclure (minuscules, majuscules, chiffres et caractères spéciaux).",
+    langage: ["react", "vite", "tailwind"],
+    githubUrl: "https://github.com/Eliott133/password_generator",
+    demoUrl: "https://generate-your-password-eliott.netlify.app/"
+  },
+  {
+    preview: "/project/music-together-project.webp",
+    name: "Music together",
+    description: "Application web permettant l'écoute collaborative de musique en temps réel. Les utilisateurs peuvent créer ou rejoindre une salle via un code unique, partager des liens YouTube pour diffuser des morceaux synchronisés et discuté via un chat intégré.",
+    langage: ["react", "nodejs", "tailwind"],
+    githubUrl: "https://github.com/Eliott133/music-together",
+  },
+];
